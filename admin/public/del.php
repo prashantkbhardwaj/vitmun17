f8c2269cc8692675291f341bbae4837abdbb66d1
Prashant <pkpbhardwaj729@gmail.com>
2017-02-14 13:54:53 +0530

    range fix

--- admin/public/del.php
+++ admin/public/del.php
@@ -22,7 +22,7 @@
     $result = mysqli_query($conn, $query);
     confirm_query($result); 
 
-    $query1 = "SELECT * FROM delegates WHERE allot = 0 AND id > 297 ORDER BY id DESC";
+    $query1 = "SELECT * FROM delegates WHERE allot = 0 AND id >= 297 ORDER BY id DESC";
     $result1 = mysqli_query($conn, $query1);
     confirm_query($result1); 
 
