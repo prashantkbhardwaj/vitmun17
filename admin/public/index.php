ee5a3e2cb2504f86235948ed911e8e5d58bf080d
Prashant <pkpbhardwaj729@gmail.com>
2017-02-17 10:52:36 +0530

    allotment panel linked

--- admin/public/index.php
+++ admin/public/index.php
@@ -234,7 +234,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="#">
+                            <a href="del.php#allot">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
