d069b1824ed9ffb5c18c0478e7939249d888e56a
Prashant <pkpbhardwaj729@gmail.com>
2017-02-27 11:35:47 +0530

    mail date

--- admin/public/del_mail2.php
+++ admin/public/del_mail2.php
@@ -13,12 +13,6 @@ $email = $title['email'];
 $name = $title['name'];
 $allot_council = $title['allot_council'];
 $allot_country = $title['allot_country'];
-if ($title['in_out']==0) {
-	$in_out = "Click <a href='http://vitcmun.com/payment_select.php?del_id=".$del_id."'><b>here</b></a> to pay the delegate fee of Rs. 1341.2 inclusive of all taxes and confirm your seat. Please <b>DO NOT</b> change the email given in the payment portal. Let it be as pkpbhardwaj729@gmail.com. Since you are an internal delegate, you also have an option to pay the fee of Rs. 1300 on our payment desks around the campus.";
-} elseif ($title['in_out']==1) {
-	$in_out = "Click <a href='http://vitcmun.com/payment_select.php?del_id=".$del_id."'><b>here</b></a> to pay the delegate fee of Rs. 1341.2 inclusive of all taxes and confirm your seat. Please <b>DO NOT</b> change the email given in the payment portal. Let it be as pkpbhardwaj729@gmail.com.";
-}
-
 
 // registration bill html code starts
 
@@ -32,7 +26,7 @@ $content .= "<p> ";
 $content .= "<b>Dear ".ucfirst($name)."</b> ";
 $content .= "</p><br><br> ";
 $content .= "<p> ";
-$content .= "Congratulations! You have been allotted <b>".$allot_council."</b> as <b>".$allot_country."</b>. ".$in_out."<br>The last day of fee payment is 3rd of March. Please complete your payment before the deadline to confirm your participation.<br>We look forward to see you in the council. :) ";
+$content .= "Due to some technical issue, the last date of payment for round 2 was misinformed. The deadline for the payment of your delegate fee is <b>3rd of March, 2017.</b><br>Sorry for the inconvinience caused.";
 $content .= "</p><br><br> ";
 $content .= "<p> ";
 $content .= "<b>Regards<br>Prashant Bhardwaj<br>Technical Head<br>VITCMUN 2017</b> ";
@@ -58,7 +52,7 @@ $mail->addAddress("$email");
 $mail->WordWrap = 50; 
 $mail->isHTML(true);                                  
  
-$mail->Subject = 'Regarding your application for VITCMUN 2017';
+$mail->Subject = 'Change in the payment deadline';
 $mail->Body    = $content;
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
@@ -68,5 +62,5 @@ if(!$mail->send()) {
    exit;
 } 
 
-	redirect_to("del.php?status=1");
+	//redirect_to("del.php?status=1");
 ?>
