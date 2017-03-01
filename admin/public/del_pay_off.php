3f945a038eab22a18c9de0f4f198ba993a0370bc
Prashant <pkpbhardwaj729@gmail.com>
2017-03-01 17:33:42 +0530

    final test off pay

--- admin/public/del_pay_off.php
+++ admin/public/del_pay_off.php
@@ -4,6 +4,8 @@
 <?php confirm_logged_in(); ?>
 <?php 
 $del_id = $_GET["del_id"];
+$page_id = $_GET['page_id'];
+$page = "council.php?eb_id=".$page_id."&status=1";
 
 $query = "SELECT * FROM delegates WHERE id = {$del_id} LIMIT 1";
 $result = mysqli_query($conn, $query);
@@ -18,54 +20,55 @@ $update_query = "UPDATE delegates SET pay_status = 1, pay_type = 2 WHERE id = {$
 $update_result = mysqli_query($conn, $update_query);
 confirm_query($update_result);
 
+if ($update_result) {
+	$content = "<!DOCTYPE html> ";
+	$content .= "<html> ";
+	$content .= "<head> ";
+	$content .= "<title>Acceptance</title> ";	
+	$content .= "</head> ";
+	$content .= "<body style='overflow: hidden;'> ";
+	$content .= "<p> ";
+	$content .= "<b>Dear ".ucfirst($name)."</b> ";
+	$content .= "</p><br><br> ";
+	$content .= "<p> ";
+	$content .= "Congratulations! You have been allotted <b>".$allot_council."</b> as <b>".$allot_country."</b>. Your payment and participation has been <b>confirmed</b>. <br>We look forward to see you in the council. :) ";
+	$content .= "</p><br><br> ";
+	$content .= "<p> ";
+	$content .= "<b>Regards<br>Prashant Bhardwaj<br>Technical Head<br>VITCMUN 2017</b> ";
+	$content .= "</p> ";
+	$content .= "</body> ";
+	$content .= "</html>";
 
-// registration bill html code starts
+	// registration bill html ends
 
-$content = "<!DOCTYPE html> ";
-$content .= "<html> ";
-$content .= "<head> ";
-$content .= "<title>Acceptance</title> ";	
-$content .= "</head> ";
-$content .= "<body style='overflow: hidden;'> ";
-$content .= "<p> ";
-$content .= "<b>Dear ".ucfirst($name)."</b> ";
-$content .= "</p><br><br> ";
-$content .= "<p> ";
-$content .= "Congratulations! You have been allotted <b>".$allot_council."</b> as <b>".$allot_country."</b>. Your payment and participation has been <b>confirmed</b>. <br>We look forward to see you in the council. :) ";
-$content .= "</p><br><br> ";
-$content .= "<p> ";
-$content .= "<b>Regards<br>Prashant Bhardwaj<br>Technical Head<br>VITCMUN 2017</b> ";
-$content .= "</p> ";
-$content .= "</body> ";
-$content .= "</html>";
+	require 'PHPMailer-master/PHPMailerAutoload.php';
 
-// registration bill html ends
+	$mail = new PHPMailer;
+	 
+	$mail->isSMTP();                                      
+	$mail->Host = 'smtp.gmail.com';                       
+	$mail->SMTPAuth = true;                               
+	$mail->Username = 'VITCMUN2017@gmail.com';                   
+	$mail->Password = '25nov1992';               
+	$mail->SMTPSecure = 'tls';                            
+	$mail->Port = 587;                                    
+	$mail->setFrom('VITCMUN2017@gmail.com', 'VITCMUN 2017');
+	$mail->addAddress("$email");       
+	$mail->WordWrap = 50; 
+	$mail->isHTML(true);                                  
+	 
+	$mail->Subject = 'Regarding your application for VITCMUN 2017';
+	$mail->Body    = $content;
+	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-require 'PHPMailer-master/PHPMailerAutoload.php';
+	if(!$mail->send()) {
+	   echo 'Message could not be sent.';
+	   echo 'Mailer Error: ' . $mail->ErrorInfo;
+	   exit;
+	} 
 
-$mail = new PHPMailer;
- 
-$mail->isSMTP();                                      
-$mail->Host = 'smtp.gmail.com';                       
-$mail->SMTPAuth = true;                               
-$mail->Username = 'VITCMUN2017@gmail.com';                   
-$mail->Password = '25nov1992';               
-$mail->SMTPSecure = 'tls';                            
-$mail->Port = 587;                                    
-$mail->setFrom('VITCMUN2017@gmail.com', 'VITCMUN 2017');
-$mail->addAddress("$email");       
-$mail->WordWrap = 50; 
-$mail->isHTML(true);                                  
- 
-$mail->Subject = 'Regarding your application for VITCMUN 2017';
-$mail->Body    = $content;
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	redirect_to($page);	
+}
 
-if(!$mail->send()) {
-   echo 'Message could not be sent.';
-   echo 'Mailer Error: ' . $mail->ErrorInfo;
-   exit;
-} 
 
-	//redirect_to("del.php?status=1");
 ?>
