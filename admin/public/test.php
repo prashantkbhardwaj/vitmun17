af08e822bbda5574dc1c401060b8d4290978e50f
Prashant <pkpbhardwaj729@gmail.com>
2017-03-05 10:28:22 +0530

    payemnt total

--- admin/public/test.php
+++ admin/public/test.php
@@ -2,24 +2,28 @@
 <?php require_once("../../includes/db_connection.php");?>
 <?php require_once("../../includes/functions.php");?>
 <?php
-	if (isset($_POST['submit'])) {
-		$val = htmlentities($_POST['ch']);
-		$query = "INSERT INTO councils (name) VALUE('{$val}')";
-		$result = mysqli_query($conn, $query);
-	}
+	$total_query = "SELECT COUNT(id) FROM delegates WHERE pay_status = 1";
+	$total_result = mysqli_query($conn, $$total_query);
+	confirm_query($total_result);
+	$total = mysqli_fetch_array($total_result);
+   	echo "Total collection: ".$total[0];  
+
+   	$online_query = "SELECT COUNT(id) FROM delegates WHERE pay_status = 1 AND pay_type = 1";
+	$online_result = mysqli_query($conn, $$online_query);
+	confirm_query($online_result);
+	$online = mysqli_fetch_array($online_result);
+   	echo "online collection: ".$online[0];  
+
+   	$paytm_query = "SELECT COUNT(id) FROM delegates WHERE pay_status = 1 AND pay_type = 3";
+	$paytm_result = mysqli_query($conn, $$paytm_query);
+	confirm_query($paytm_result);
+	$paytm = mysqli_fetch_array($paytm_result);
+   	echo "paytm collection: ".$paytm[0];  
+
+   	$offline_query = "SELECT COUNT(id) FROM delegates WHERE pay_status = 1 AND pay_type = 2";
+	$offline_result = mysqli_query($conn, $$offline_query);
+	confirm_query($offline_result);
+	$offline = mysqli_fetch_array($offline_result);
+   	echo "offline collection: ".$offline[0];  
 	
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<form method="post" action="test.php">
-		<select name="ch">
-			<option value="Meghan L. O’Sullivan">Meghan L. O’Sullivan</option>
-		</select>
-		<input type="submit" name="submit" value="submit">
-	</form>
-</body>
-</html>
